b3edd31b5d35cbbce04fd70accc0311beaed5ab5
Filipe Cristiano <filipe.cristiano@uaify.com>
2025-06-29 21:18:04 -0300

    feat: configuraçao inicial do fire base com as rotas e estilos iniciais

--- 10_MINI_BLOG/mini_blog/src/App.jsx
+++ 10_MINI_BLOG/mini_blog/src/App.jsx
@@ -1,11 +1,26 @@
-import './App.css'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import Home from "./pages/Home";
+import About from "./pages/About";
+
+import NavBar from "./components/NavBar";
+import Footer from "./components/Footer";
+
+import "./App.css";
 
 function App() {
   return (
-    <>
-      <h1>Mini Blog</h1>
-    </>
-  )
+    <BrowserRouter>
+      <NavBar />
+      <div className="container">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="About" element={<About />} />
+        </Routes>
+      </div>
+      <Footer />
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
---
+++ 10_MINI_BLOG/mini_blog/src/components/NavBar/index.jsx
@@ -0,0 +1,33 @@
+import { NavLink } from "react-router-dom";
+
+import styles from "./NavBar.module.css";
+
+const NavBar = () => {
+  return (
+    <nav className={styles.navbar}>
+      <NavLink className={styles.brand} to="/">
+        Mini <span>Blog</span>
+      </NavLink>
+      <ul className={styles.links_list}>
+        <li>
+          <NavLink
+            to="/"
+            className={({ isActive }) => (isActive ? styles.active : "")}
+          >
+            Home
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="/about"
+            className={({ isActive }) => (isActive ? styles.active : "")}
+          >
+            About
+          </NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default NavBar;
